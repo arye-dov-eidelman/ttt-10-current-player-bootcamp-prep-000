2aa3db218463d4441607cd4c7518b7eb9dae0bea
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-14 21:40:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
 def turn_count(board)
-  each.board do |cell|
+  each.board do |index|
+    position_taken?(board, location)
+  end
   
 end
 
